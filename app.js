9977d80d0282f175e2ce4dc1b55885b160c8b3a5
emmy <emilceespinoza22@gmail.com>
2021-07-29 16:45:37 -0300

    estructura

--- app.js
+++ app.js
@@ -5,21 +5,27 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.get('/registrarse', (req, res)=>{
+app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
-app.get('/', (req, res)=>{
+app.get('/footer', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/footer.html'))
 })
-
+app.get('/header', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/header.html'))
+})
 app.get('/registrarse', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/registrarse.html'))
 })
-
 app.get('/iniciar-sesion', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/views/footer.html'))
+    res.sendFile(path.join(__dirname, '/views/iniciar-sesion'))
 })
-
+app.get('/carrito', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/carrito'))
+})
+app.get('/producto', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/producto'))
+})
 app.listen(port, () => {
     console.log(`Puerto corriendo en ${port}\n http://localhost:${port}`)
 });
