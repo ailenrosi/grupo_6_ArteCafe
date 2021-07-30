a003ce9a401418335e7e0cdb92124da372587489
emmy <emilceespinoza22@gmail.com>
2021-07-30 12:46:33 -0300

    modifico app

--- app.js
+++ app.js
@@ -8,26 +8,27 @@ app.use(express.static('public'));
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
-app.get('/error', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/views/error_404.html'))
-})
-
-app.get('/header', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/views/header.html'))
-})
-
 app.get('/footer', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/footer.html'))
 })
-
+app.get('/header', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/header.html'))
+})
 app.get('/registrarse', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/registrarse.html'))
 })
-
 app.get('/iniciar-sesion', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/iniciar-sesion.html'))
 })
-
+app.get('/carrito', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/carrito.html'))
+})
+app.get('/producto', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/producto.html'))
+})
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/error_404.html'))
+})
 app.listen(port, () => {
     console.log(`Puerto corriendo en ${port}\n http://localhost:${port}`)
 });
