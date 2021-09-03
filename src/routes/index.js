e228364fe5b0d046308d59fbbeaf21a28aef4d6c
emmy <emilceespinoza22@gmail.com>
2021-09-02 21:23:47 -0300

    index

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,6 @@ router.get('/', controller.index);
 
 router.get('/contact', controller.contact);
 
-<<<<<<< HEAD
-router.get('/admin', admin);
-
-module.exports = router;
-=======
->>>>>>> 3b90f6becc48db658d457fdc6956e12c9bd5dc5c
 router.get('/search', controller.search); 
 
 router.get('/sobre_nosotros', controller.sobreNosotros);
