10a8ae0fde354f59e65944bf297249f9c2aa1528
emmy <emilceespinoza22@gmail.com>
2021-09-09 17:33:56 -0300

    adm

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,5 +1,29 @@
-module.exports = {
+const fs = require("fs");
+const path = require("path");
+const products = JSON.parse(fs.readFileSync(path.join(__dirname,"..","data","products.json"),"utf-8"));
+let save = (dato) => fs.writeFileSync(path.join(__dirname,'..','data','products.json'),JSON.stringify(dato,null,2),'utf-8') /* gurada en el json products */
+
+
+module.exports ={
     admin: (req, res) => {
         res.render('admin');
-    }
+    },
+
+	products: (req,res) => {
+		const {name, price, category} = req.body
+		let  product = {
+			name,
+			price,
+			category
+		}
+		product
+	},
+    
+	list: (req, res) => {
+		return res.render("listProducts",{products})
+	}
+	
 }
+
+
+
