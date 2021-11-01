2bbcde2625f95fb1d622cd3a97f548b752efd85b
Ailenrosi <rorosiailu@gmailcom>
2021-11-01 19:32:33 -0300

    validaciones

--- public/js/register.js
+++ public/js/register.js
@@ -1,61 +1,214 @@
-window.addEventListener('load', () => {
 
+function qs(element) {
+    return document.querySelector(element);
+  }
+  
+  window.addEventListener("load", function () {
+    let $inputName = qs("#name"),
+      $nameErrors = qs("#nameErrors"),
 
-    const iniciarActive = () => {
-        document.getElementById("is").classList.add("active");
-        document.getElementById("cc").classList.remove("active");
-        document.getElementById("inicio-sesion").classList.remove("none");
-        document.getElementById("crear-cuenta").classList.add("none");
-    }
-    
-    const crearActive = () => {
-        document.getElementById("cc").classList.add("active");
-        document.getElementById("is").classList.remove("active");
-        document.getElementById("crear-cuenta").classList.remove("none");
-        document.getElementById("inicio-sesion").classList.add("none");
-    }
-    
-    
-    document.getElementById("is").onclick = () => {
-        iniciarActive();
-    }
-    
-    document.getElementById("cc").onclick = () => {
-        crearActive();
-    }
-    
-    
-    /* VIEW PASSWORD */
-    
-    let btn1 = document.getElementById('btn-pass-1');
-    let btn2 = document.getElementById('btn-pass-2');
-    let btn3 = document.getElementById('btn-pass-3');
-    
-    let pass = document.getElementById('passwordInicio');
-    let pass2 = document.getElementById('passwordRegistro');
-    let pass3 = document.getElementById('passwordRepeat');
-    
-    function mostrarConstraseña(pass, btn){
-        if(pass.type == "password"){
-            pass.type = "text";
-            btn.classList.remove("fa-eye");
-            btn.classList.add("fa-eye-slash")
-        } else {
-            pass.type = "password";
-            btn.classList.remove("fa-eye-slash");
-            btn.classList.add("fa-eye")
-        }
-    }
-    
-    btn1.onclick = () => {
-        mostrarConstraseña(pass, btn1)
-    }
-    
-    btn2.onclick = () => {
-        mostrarConstraseña(pass2, btn2)
-    }
-    
-    btn3.onclick = () => {
-        mostrarConstraseña(pass3, btn3)
-    }
-})
+      $inputLastname = qs("#lastname"),
+      $lastnameErrors = qs("#lastnameErrors"),
+
+      $form = qs("#form"),
+
+      $dni = qs("#dni"),
+      $dniErrors = qs("#dniErrors"),
+
+      $email = qs("#email"),
+      $emailErrors = qs("#emailErrors"),
+
+      $pass = qs("#pass"),
+      $passErrors = qs("#passErrors"),
+
+      $pass2 = qs("#pass2"),
+      $pass2Errors = qs("#pass2Errors"),
+
+      $terms = qs("#flexCheckDefault"),
+      $termsErrors = qs("#termsErrors"),
+
+      $file = qs("#formFile"),
+      $fileErrors = qs("#fileErrors"),
+
+      $imgPreview = qs("#img-preview"),
+      
+      regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/,
+      regExDNI = /^[0-9]{7,8}$/,
+      regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+      regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+  
+    $inputName.addEventListener("blur", function () {
+      switch (true) {
+        case !$inputName.value.trim():
+          $nameErrors.innerHTML = "El campo nombre es obligatorio";
+          $inputName.classList.add("is-invalid");
+          break;
+        case !regExAlpha.test($inputName.value):
+          $nameErrors.innerHTML = "Debes ingresar un nombre válido";
+          $inputName.classList.add("is-invalid");
+          break;
+        default:
+          $inputName.classList.remove("is-invalid");
+          $inputName.classList.add("is-valid");
+          $nameErrors.innerHTML = "";
+          break;
+      }
+    });
+  
+    $inputLastname.addEventListener("blur", function () {
+        console.log("hola")
+      switch (true) {
+        case !$inputLastname.value.trim():
+          $lastnameErrors.innerHTML = "El campo apellido es obligatorio";
+          $inputLastname.classList.add("is-invalid");
+          break;
+        case !regExAlpha.test($inputLastname.value):
+          $lastnameErrors.innerHTML = "Debes ingresar un apellido válido";
+          $inputLastname.classList.add("is-invalid");
+          break;
+        default:
+          $inputLastname.classList.remove("is-invalid");
+          $inputLastname.classList.add("is-valid");
+          $lastnameErrors.innerHTML = "";
+          break;
+      }
+    });
+  
+    $dni.addEventListener('blur', function() {
+      switch (true) {
+          case !$dni.value.trim():
+              $dniErrors.innerHTML = 'El campo DNI es obligatorio';
+              $dni.classList.add('is-invalid')
+              break;
+          case !regExDNI.test($dni.value):
+              $dniErrors.innerHTML = 'Debe ingresar un dni válido';
+              $dni.classList.add('is-invalid')
+              break
+          default:
+              $dni.classList.remove('is-invalid');
+              $dni.classList.add('is-valid');
+              $dniErrors.innerHTML = ''
+              break;
+      }
+  })
+  
+  $email.addEventListener('blur', function() {
+      switch (true) {
+          case !$email.value.trim():
+              $emailErrors.innerHTML = 'El campo email es obligatorio';
+              $email.classList.add('is-invalid')
+              break;
+          case !regExEmail.test($email.value):
+              $emailErrors.innerHTML = 'Debe ingresar un email válido';
+              $email.classList.add('is-invalid')
+              break
+          default:
+              $email.classList.remove('is-invalid');
+              $email.classList.add('is-valid');
+              $emailErrors.innerHTML = ''
+              break;
+      }
+  })
+  
+  $pass.addEventListener('blur', function() {
+      switch (true) {
+          case !$pass.value.trim():
+              $passErrors.innerHTML = 'El campo contraseña es obligatorio';
+              $pass.classList.add('is-invalid')
+              break;
+          case !regExPass.test($pass.value):
+              $passErrors.innerHTML = 'La contraseña debe tener: entre 6 o 12 caracteres, al menos una mayúscula, una minúscula y un número';
+              $pass.classList.add('is-invalid')
+              break
+          default:
+              $pass.classList.remove('is-invalid');
+              $pass.classList.add('is-valid');
+              $passErrors.innerHTML = ''
+              break;
+      }
+  })
+  
+  
+  $pass2.addEventListener('blur', function(){
+      switch (true) {
+          case !$pass2.value.trim():
+              $pass2Errors.innerHTML = 'Debes reingresar la contraseña';
+              $pass2.classList.add('is-invalid')
+              break;
+          case $pass2.value != $pass.value:
+              $pass2Errors.innerHTML = 'Las contraseñas no coinciden';
+              $pass2.classList.add('is-invalid')
+              break;
+          default:
+              $pass2.classList.remove('is-invalid');
+              $pass2.classList.add('is-valid');
+              $pass2Errors.innerHTML = ''
+              break;
+      }
+  })
+  
+   $terms.addEventListener('click',function(){
+      $terms.value = 'on'
+      $terms.classList.toggle('is-valid');
+      $terms.classList.remove('is-invalid');
+      $termsErrors.innerHTML = ""
+  })
+  
+  
+  $form.addEventListener('submit',function(event){
+      let error = false;
+      event.preventDefault()
+      console.log($form.elements)
+      let elementosForm = this.elements
+      
+      for (let index = 0; index < elementosForm.length-1; index++) {
+          if(elementosForm[index].value == "" && elementosForm[index].name !== "archivo"){
+              elementosForm[index].classList.add('is-invalid');
+              submitErrors.innerHTML = "Los campos señalados son obligatorios";
+              error = true;
+          }
+      }
+  
+      if(!$terms.checked){
+          $terms.classList.add('is-invalid');
+          $termsErrors.innerHTML = "Debes aceptar las bases y condiciones"
+          error = true
+      }
+  
+      if(!error){
+          console.log('Todo bien');
+          $form.submit()
+      }
+  
+  })
+  
+  $file.addEventListener('change', 
+      function fileValidation(){
+          let filePath = $file.value, //Capturo el valor del input
+              allowefExtensions = /(.jpg|.jpeg|.png|.gif|.web)$/i //Extensiones permitidas
+          if(!allowefExtensions.exec(filePath)){ //El método exec() ejecuta una busqueda sobre las coincidencias de una expresión regular en una cadena especifica. Devuelve el resultado como array, o null.
+              $fileErrors.innerHTML = 'Carga un archivo de imagen válido, con las extensiones (.jpg - .jpeg - .png - .gif)';
+              $file.value = '';
+              $imgPreview.innerHTML = '';
+              return false;
+          }else{
+              // Image preview
+              console.log($file.files);
+              if($file.files && $file.files[0]){
+                  let reader = new FileReader();
+                  reader.onload = function(e){
+                      $imgPreview.innerHTML = '<img src="' + e.target.result +'"/>';
+                  };
+                  reader.readAsDataURL($file.files[0]);
+                  $fileErrors.innerHTML = '';
+                  $file.classList.remove('is-invalid')
+              }
+          }
+      })
+  
+  
+  
+  
+  
+  });
+  
--- public/js/user.js
+++ public/js/user.js
@@ -1,47 +1,3 @@
-/* 
-const nombre = document.getElementById("name")
-const email = document.getElementById("email")
-const pass = document.getElementById("password")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
-
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let warnings = ""
-    let entrar = false
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-    parrafo.innerHTML = ""
-    if(nombre.value.length <6){
-        warnings += `El nombre no es valido <br>`
-        entrar = true
-    }
-    if(!regexEmail.test(email.value)){
-        warnings += `El email no es valido <br>`
-        entrar = true
-    }
-    if(pass.value.length < 8){
-        warnings += `La contraseña no es valida <br>`
-        entrar = true
-    }
-
-    if(entrar){
-        parrafo.innerHTML = warnings
-    }else{
-        parrafo.innerHTML = "Enviado"
-    }
-})
-
-function myFunction() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
-  
-
- */
 
   function qs(element) {
     return document.querySelector(element);
@@ -53,7 +9,7 @@ function myFunction() {
       $pass = qs("#pass"),
       $passErrors = qs("#passErrors"),
       regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
-      regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+      regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,12}$/;
   
       $email.addEventListener('blur', function() {
         switch (true) {
@@ -80,7 +36,8 @@ function myFunction() {
                 $pass.classList.add('is-invalid')
                 break;
             case !regExPass.test($pass.value):
-                $passErrors.innerHTML = 'La contraseña debe tener: entre 6 o 12 caracteres, al menos una mayúscula, una minúscula y un número';
+                $passl
+Errors.innerHTML = 'Debe ingresar una contraseña válida';
                 $pass.classList.add('is-invalid')
                 break
             default:
