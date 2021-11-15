fa4ebbfc0885e28abd5b5ae4f98b445d26dd4090
emmy <emilceespinoza22@gmail.com>
2021-11-15 18:09:45 -0300

    eliminar producto

--- public/js/header.js
+++ public/js/header.js
@@ -16,3 +16,4 @@ document.querySelector('#search-icon').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
   document.querySelector('#search-form').classList.remove('active');
 }
+
