31e5b3f08f827456c2d2d28f023fc3f0d0e94371
Ailenrosi <rorosiailu@gmailcom>
2021-11-18 13:19:39 -0300

    Animacion Info Cursos

--- public/js/curso.js
+++ public/js/curso.js
@@ -1,1 +1,9 @@
-alert("OK");
+$(document).ready(()=> {
+    $("#baristaCompleto_info").hide();
+})
+
+let baristaCompleto_btn = $("#baristaCompleto_btn");
+
+baristaCompleto_btn.click(() => {
+    $("#baristaCompleto_info").slideToggle("slow");
+});
