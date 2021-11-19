8aa152e526205fb69d53a33ec9f0f324d49b759a
Ailenrosi <rorosiailu@gmailcom>
2021-11-19 20:49:47 -0300

    arreglando vista de cursos

--- public/js/curso.js
+++ public/js/curso.js
@@ -7,3 +7,54 @@ let baristaCompleto_btn = $("#baristaCompleto_btn");
 baristaCompleto_btn.click(() => {
     $("#baristaCompleto_info").slideToggle("slow");
 });
+
+$(document).ready(()=> {
+    $("#cafeFiltrado_info").hide();
+})
+
+let cafeFiltrado_btn = $("#cafeFiltrado_btn");
+
+cafeFiltrado_btn.click(() => {
+    $("#cafeFiltrado_info").slideToggle("slow");
+});
+
+$(document).ready(()=> {
+    $("#espressoPerfecto_info").hide();
+})
+
+let espressoPerfecto_btn = $("#espressoPerfecto_btn");
+
+espressoPerfecto_btn.click(() => {
+    $("#espressoPerfecto_info").slideToggle("slow");
+});
+
+$(document).ready(()=> {
+    $("#arteLatte_info").hide();
+})
+
+let arteLatte_btn = $("#arteLatte_btn");
+
+arteLatte_btn.click(() => {
+    $("#arteLatte_info").slideToggle("slow");
+});
+
+$(document).ready(()=> {
+    $("#cafeFrio_info").hide();
+})
+
+let cafeFrio_btn = $("#cafeFrio_btn");
+
+cafeFrio_btn.click(() => {
+    $("#cafeFrio_info").slideToggle("slow");
+});
+
+
+$(document).ready(()=> {
+    $("#tostador_info").hide();
+})
+
+let tostador_btn = $("#tostador_btn");
+
+tostador_btn.click(() => {
+    $("#tostador_info").slideToggle("slow");
+});
